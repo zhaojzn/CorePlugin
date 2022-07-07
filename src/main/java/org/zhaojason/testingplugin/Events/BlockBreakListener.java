755f049c5b889c36119654fc06fd949cd51ecd9b
Jason Zhao <jasonnope123@gmail.com>
2022-07-06 19:11:28 -0700

    fixed null pointer execption

--- src/main/java/org/zhaojason/testingplugin/Events/BlockBreakListener.java
+++ src/main/java/org/zhaojason/testingplugin/Events/BlockBreakListener.java
@@ -23,37 +23,41 @@ public class BlockBreakListener implements Listener {
     @EventHandler
     public void onBlockBreak(BlockBreakEvent e){
         Player player = e.getPlayer();
-        ItemStack i =  e.getPlayer().getInventory().getItemInMainHand();
-        NBTItem nbti = new NBTItem(i);
+        if(e.getPlayer().getInventory().getItemInMainHand() != null)
+        {
+            ItemStack i =  e.getPlayer().getInventory().getItemInMainHand();
+            NBTItem nbti = new NBTItem(i);
+            if(i.getItemMeta() != null) {
+                if(i.getItemMeta().hasDisplayName()){
+                    if(nbti.hasKey("PickaxeXP")){
+                        e.setDropItems(false);
+                        Integer nbti_xp = nbti.getInteger("PickaxeXP");
+                        nbti_xp = nbti_xp + 1;
+                        nbti.setInteger("PickaxeXP", nbti_xp);
+                        PickaxeUtils p = new PickaxeUtils(main);
 
-        if(i.getItemMeta() != null) {
-            if(i.getItemMeta().hasDisplayName()){
-                if(nbti.hasKey("PickaxeXP")){
-                    e.setDropItems(false);
-                    Integer nbti_xp = nbti.getInteger("PickaxeXP");
-                    nbti_xp = nbti_xp + 1;
-                    nbti.setInteger("PickaxeXP", nbti_xp);
-                    PickaxeUtils p = new PickaxeUtils(main);
+                        if(p.isUpgradeable(nbti)){
+                            nbti = p.levelUpPickaxe(nbti);
+                            e.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', "&7Your pickaxe has been leveled up!"));
+                        }
+                        List<String> lore = p.updateLore(nbti);
 
-                    if(p.isUpgradeable(nbti)){
-                        nbti = p.levelUpPickaxe(nbti);
-                        e.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', "&7Your pickaxe has been leveled up!"));
+                        ItemStack finalItem = nbti.getItem();
+                        ItemMeta finalItemMeta = finalItem.getItemMeta();
+                        finalItemMeta.setLore(lore);
+                        finalItem.setItemMeta(finalItemMeta);
+
+                        player.getInventory().setItemInMainHand(finalItem);
+                    }else{
+                        e.setCancelled(true);
+                        player.sendMessage("You have a broken pickaxe");
+
                     }
-                    List<String> lore = p.updateLore(nbti);
-
-                    ItemStack finalItem = nbti.getItem();
-                    ItemMeta finalItemMeta = finalItem.getItemMeta();
-                    finalItemMeta.setLore(lore);
-                    finalItem.setItemMeta(finalItemMeta);
-
-                    player.getInventory().setItemInMainHand(finalItem);
-                }else{
-                    e.setCancelled(true);
-                    player.sendMessage("You have a broken pickaxe");
-
                 }
             }
         }
+
+
 
 
     }
